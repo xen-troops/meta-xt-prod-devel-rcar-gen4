xen-troops/meta-xt-prod-devel-rcar-gen4

commit 90b2118d8a07d62bf8ee1795ffc4243a4f53c097

xen-tools: add xenpcid (xl pcid) service package
Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

meta-xt-domx-gen4/recipes-extended/xen/xen-tools_git.bbappend
@@ -2,9 +2,17 @@ require xen-4.16-dunfell.inc
 
 PACKAGES_append = "\
     ${PN}-test \
+    ${PN}-pcid \
 "
 
 FILES_${PN}-test = "\
     ${libdir}/xen/bin/test-xenstore \
     ${libdir}/xen/bin/test-resource \
 "
+
+FILES_${PN}-pcid = "\
+    ${systemd_unitdir}/system/xenpcid.service \
+"
+
+SYSTEMD_SERVICE_${PN}-pcid = "xenpcid.service"
+

commit d65b5823e89c81de1a4686b0f26c19c8efa9f353

xen-tools: enable xenpcid systemd service
xenpcid systemd service was not enabled thus not starting during system
boot. Fix this by adding xenpcid to xen-tools's systemd packages.

Fixes: 90b2118d8a07 ("xen-tools: add xenpcid (xl pcid) service package")

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

meta-xt-domx-gen4/recipes-extended/xen/xen-tools_git.bbappend
@@ -23,6 +23,8 @@ FILES_${PN}-pcid = "\
 
 SYSTEMD_SERVICE_${PN}-pcid = "xenpcid.service"
 
+SYSTEMD_PACKAGES += "${PN}-pcid"
+
 do_install_append() {
     install -d ${D}${systemd_unitdir}/system/xenpcid.service.d/
     install -m 0744 ${WORKDIR}/xenpcid-xenstore.conf ${D}${systemd_unitdir}/system/xenpcid.service.d