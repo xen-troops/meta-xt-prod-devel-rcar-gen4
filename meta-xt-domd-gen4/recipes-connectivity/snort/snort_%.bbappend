xen-troops/meta-xt-prod-devel-rcar-gen4

commit 52c4b2b798239cb06dcaf81376a38e48777c3281

snort: handle tsn2 as spider-specific
As for now, this recipe is intended to be used for two Gen4
boards: spider and s4sk. And tsn2 is not available on s4sk.

So, this commit apply appropriate machine overrides to things
related to tsn2.

Signed-off-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>
Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

meta-xt-domd-gen4/recipes-connectivity/snort/snort_%.bbappend
@@ -6,8 +6,11 @@ SRC_URI:remove = "file://configure.in-disable-tirpc-checking-for-fedora.patch"
 
 SRC_URI += " file://snort-tsn0.service \
     file://snort-tsn1.service \
-    file://snort-tsn2.service \
     file://configure.in-disable-tirpc-checking-for-fedora-snort-20.patch \
+"
+
+SRC_URI:append:spider = " \
+    file://snort-tsn2.service \
 "
 
 FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
@@ -17,18 +20,25 @@ do_install:append() {
     install -d ${D}${systemd_unitdir}/system
     install -m 0644 ${WORKDIR}/snort-tsn0.service ${D}${systemd_unitdir}/system/
     install -m 0644 ${WORKDIR}/snort-tsn1.service ${D}${systemd_unitdir}/system/
-    install -m 0644 ${WORKDIR}/snort-tsn2.service ${D}${systemd_unitdir}/system/
     echo "config daq: rswitch_offload" >> ${D}/etc/snort/snort.conf
 }
 
+do_install:append:spider() {
+    install -d ${D}${systemd_unitdir}/system
+    install -m 0644 ${WORKDIR}/snort-tsn2.service ${D}${systemd_unitdir}/system/
+}
 
-SYSTEMD_PACKAGES = "snort-tsn0 snort-tsn1 snort-tsn2 "
+SYSTEMD_PACKAGES = "snort-tsn0 snort-tsn1"
+SYSTEMD_PACKAGES:append:spider = " snort-tsn2"
 SYSTEMD_SERVICE:snort-tsn0 = "snort-tsn0.service"
 SYSTEMD_SERVICE:snort-tsn1 = "snort-tsn1.service"
 SYSTEMD_SERVICE:snort-tsn2 = "snort-tsn2.service"
 
 FILES:${PN} += " ${systemd_unitdir}/system/snort-tsn0.service \
     ${systemd_unitdir}/system/snort-tsn1.service \
+"
+
+FILES:${PN}:append:spider += " \
     ${systemd_unitdir}/system/snort-tsn2.service \
 "
 

commit 35022b08bb959f950b50fef864c39263213631d9

domd: snort: Fix misprint in FILES
Fix incorrect appending that results in warning
```
WARNING: FILES:${PN}:append:spider += is not
a recommended operator combination, please replace it.
```

Fixes: 52c4b2b79823 ("snort: handle tsn2 as spider-specific")

Signed-off-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>
Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

meta-xt-domd-gen4/recipes-connectivity/snort/snort_%.bbappend
@@ -38,7 +38,7 @@ FILES:${PN} += " ${systemd_unitdir}/system/snort-tsn0.service \
     ${systemd_unitdir}/system/snort-tsn1.service \
 "
 
-FILES:${PN}:append:spider += " \
+FILES:${PN}:append:spider = " \
     ${systemd_unitdir}/system/snort-tsn2.service \
 "
 