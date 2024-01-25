xen-troops/meta-xt-prod-devel-rcar-gen4

commit 145ee566cc6186740bd17f8bb0828c05948da14b

Replace override separator `_` with `:`
This change makes recipes compatible with dunfell and kirkstone,
thus reducing future diff.

Signed-off-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>
Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

meta-xt-driver-domain-gen4/recipes-connectivity/xen-network/xen-network.bbappend
@@ -1,4 +1,4 @@
-FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
 
 SRC_URI += "\
     file://systemd-networkd-wait-online.conf \

meta-xt-dom0-gen4/recipes-kernel/linux/linux-generic-armv8.bbappend
@@ -1,4 +1,4 @@
-FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
 BRANCH = "v5.10.41/rcar-5.1.7.rc5"
 SRCREV = "${AUTOREV}"

meta-xt-domd-gen4/recipes-bsp/arm-trusted-firmware/arm-trusted-firmware_git.bbappend
@@ -1,4 +1,4 @@
-FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
 
 # Start cores in EL2 mode
 SRC_URI += "\

meta-xt-domd-gen4/recipes-bsp/u-boot/u-boot_2020.10.bbappend
@@ -1,4 +1,4 @@
-FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
 
 SRC_URI += " \
     file://0001-net-rswtich-disable-GWCA-when-removing-driver.patch \

meta-xt-domd-gen4/recipes-connectivity/snort/snort_%.bbappend
@@ -2,7 +2,7 @@ PV = "2.9.20"
 SRC_URI[md5sum] = ""
 SRC_URI[sha256sum] = "29400e13f53b1831e0b8b10ec1224a1cbaa6dc1533a5322a20dd80bb84b4981c"
 
-SRC_URI_remove = "file://configure.in-disable-tirpc-checking-for-fedora.patch"
+SRC_URI:remove = "file://configure.in-disable-tirpc-checking-for-fedora.patch"
 
 SRC_URI += " file://snort-tsn0.service \
     file://snort-tsn1.service \
@@ -10,10 +10,10 @@ SRC_URI += " file://snort-tsn0.service \
     file://configure.in-disable-tirpc-checking-for-fedora-snort-20.patch \
 "
 
-FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
 
-do_install_append() {
+do_install:append() {
     install -d ${D}${systemd_unitdir}/system
     install -m 0644 ${WORKDIR}/snort-tsn0.service ${D}${systemd_unitdir}/system/
     install -m 0644 ${WORKDIR}/snort-tsn1.service ${D}${systemd_unitdir}/system/
@@ -23,11 +23,11 @@ do_install_append() {
 
 
 SYSTEMD_PACKAGES = "snort-tsn0 snort-tsn1 snort-tsn2 "
-SYSTEMD_SERVICE_snort-tsn0 = "snort-tsn0.service"
-SYSTEMD_SERVICE_snort-tsn1 = "snort-tsn1.service"
-SYSTEMD_SERVICE_snort-tsn2 = "snort-tsn2.service"
+SYSTEMD_SERVICE:snort-tsn0 = "snort-tsn0.service"
+SYSTEMD_SERVICE:snort-tsn1 = "snort-tsn1.service"
+SYSTEMD_SERVICE:snort-tsn2 = "snort-tsn2.service"
 
-FILES_${PN} += " ${systemd_unitdir}/system/snort-tsn0.service \
+FILES:${PN} += " ${systemd_unitdir}/system/snort-tsn0.service \
     ${systemd_unitdir}/system/snort-tsn1.service \
     ${systemd_unitdir}/system/snort-tsn2.service \
 "

meta-xt-domd-gen4/recipes-core/base-files/base-files_%.bbappend
@@ -2,6 +2,6 @@
 # meta-renesas does strange things with fstab file to fix own problems
 # with NFS. We don't have such problems, so we need to revert some changes
 # made by Renesas. Namely, we want /var/volatile to be owned by root.
-do_install_append() {
+do_install:append() {
     sed -i "s/uid=65534,gid=65534/defaults/" ${D}${sysconfdir}/fstab
 }

meta-xt-domd-gen4/recipes-kernel/linux/linux-renesas_%.bbappend
@@ -1,14 +1,14 @@
-FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
 RENESAS_BSP_URL = "git://github.com/xen-troops/linux.git"
 BRANCH = "${XT_KERNEL_BRANCH}"
 SRCREV = "${XT_KERNEL_REV}"
 
-SRC_URI_append = "\
+SRC_URI:append = "\
     file://defconfig \
 "
 
-SRC_URI_append = " \
+SRC_URI:append = " \
     file://r8a779f0.cfg \
     file://rswitch.cfg \
     file://dmatest.cfg \

meta-xt-domu-gen4/recipes-connectivity/vmq-network/vmq-network.bb
@@ -11,7 +11,7 @@ SRC_URI = " \
 
 S = "${WORKDIR}"
 
-FILES_${PN} = " \
+FILES:${PN} = " \
     ${sysconfdir}/systemd/network/vmq0.network \
     ${sysconfdir}/systemd/network/tsn1.network \
 "

commit f38709f3a049eaca21ee28c25813546274420301

xen-network: fix override separator
Fixes: 145ee566cc61 ("Replace override separator `_` with `:`")

Signed-off-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>
Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

meta-xt-driver-domain-gen4/recipes-connectivity/xen-network/xen-network.bbappend
@@ -7,7 +7,7 @@ SRC_URI += "\
     file://vmq0.network \
 "
 
-FILES_${PN} += " \
+FILES:${PN} += " \
     ${sysconfdir}/systemd/network/tsn0.network \
     ${sysconfdir}/systemd/network/tsn1.network \
     ${sysconfdir}/systemd/network/vmq0.network \